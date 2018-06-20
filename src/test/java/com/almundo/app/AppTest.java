0c6afc0d47cacd14e2bf39cb821021a8926cdd70
gmazzei <gmazzei91@gmail.com>
2018-06-20 18:36:40 -0300

    Test improvement

--- src/test/java/com/almundo/app/AppTest.java
+++ src/test/java/com/almundo/app/AppTest.java
@@ -18,18 +18,37 @@ public class AppTest extends TestCase {
 	
 	private static final int CALL_QUEUE_MAX_SIZE = 10;
 	
+	
+	/**
+	 * Testear que se puedan atender 10 llamadas de forma concurrente.
+	 * */
 	public void testConcurrentTasks() {
+		final int timeout = 25000;
+		long start;
+		
 		BlockingQueue<Call> callQueue = new ArrayBlockingQueue<Call>(CALL_QUEUE_MAX_SIZE, true);
     	
     	Producer producer = new Producer(callQueue);
     	new Thread(producer).start();
     	
-    	while (callQueue.size() < CALL_QUEUE_MAX_SIZE);
+    	//Esperamos a que la cola se llene
+    	start = System.currentTimeMillis();
+    	while (callQueue.size() < CALL_QUEUE_MAX_SIZE) {
+    		if (System.currentTimeMillis() - start > timeout) {
+    			throw new RuntimeException("Call queue never reached max size. Actual size: " + callQueue.size());
+    		} 
+    	}
     	
     	Dispatcher dispatcher = new Dispatcher(callQueue);
     	new Thread(dispatcher).start();
     	
-    	while (dispatcher.getBusyTasks() < 10);
+    	//El número de hilos si o si llegara a 10 en instantes.
+    	start = System.currentTimeMillis();
+    	while (dispatcher.getBusyTasks() < 10) {
+    		if (System.currentTimeMillis() - start > timeout) {
+    			throw new RuntimeException("Busy tasks never reached max number. Actual number: " + dispatcher.getBusyTasks());
+    		} 
+    	}
     	
     	assertEquals(10, dispatcher.getBusyTasks());
 	}
