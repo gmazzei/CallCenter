daa759af7a8edd8aaaad428c54203dbddfc374ac
gmazzei <gmazzei91@gmail.com>
2018-06-20 22:59:52 -0300

    Log in App class

--- src/main/java/com/almundo/app/App.java
+++ src/main/java/com/almundo/app/App.java
@@ -3,6 +3,8 @@ package com.almundo.app;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
+import org.apache.log4j.Logger;
+
 import com.almundo.model.Call;
 import com.almundo.model.Dispatcher;
 import com.almundo.model.Producer;
@@ -10,9 +12,11 @@ import com.almundo.model.Producer;
 
 public class App {
 	
+	private static Logger log = Logger.getLogger(App.class);
 	private static final int CALL_QUEUE_MAX_SIZE = 10;
 	
     public static void main(String[] args) {
+    	log.debug("Call center begins");
     	BlockingQueue<Call> callQueue = new ArrayBlockingQueue<Call>(CALL_QUEUE_MAX_SIZE, true);
     	
     	Producer producer = new Producer(callQueue);
